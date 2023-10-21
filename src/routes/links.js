37313771a5c10c121d3649ee95f04b1bb6ab9c6c
Eduardo Antonio Martinez <eduardoa4848@Outlook.es>
2023-10-21 16:32:49 -0500

    se agrega la interfaz de subida de datos, se modifican las interfaces, se completa el dashboard, se la interfas de busqueda de policia, se agrega la interfaz de busqueda de criminales, se agrega la interfas de perfil y se agrega la interfaz de agregar policia

--- src/routes/links.js
+++ src/routes/links.js
@@ -11,8 +11,28 @@ router.get('/dashboard',(req,res)=>{
     res.render('links/Dashboard/dashboard');
 });
 
+router.get('/add-user',(req,res)=>{
+    res.render('links/Dashboard/addUser');
+});
+
+router.get('/users',(req,res)=>{
+    res.render('links/Dashboard/police');
+});
+
+router.get('/criminals',(req,res)=>{
+    res.render('links/Dashboard/criminals');
+});
+
+router.get('/upload-criminals',(req,res)=>{
+    res.render('links/Dashboard/DataCaptureCriminal');
+});
+
+router.get('/profile',(req,res)=>{
+    res.render('links/Login/profile');
+});
 
 router.post('/login',(req,res)=>{
 
 });
+
 module.exports=router;
